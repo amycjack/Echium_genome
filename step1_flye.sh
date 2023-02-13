2e1ce4f34c73aa2853d0ede55de1f23b2d36ac12
Amy Jackson <86874057+amycjack@users.noreply.github.com>
2023-02-13 09:27:37 +0000

    Create step1_flye.sh

---
+++ step1_flye.sh
@@ -0,0 +1,25 @@
+
+# trimming short reads
+
+trimmomatic PE -threads 16 \
+-phred33 \
+-trimlog trim.log \
+HolEw3-1a_S1_L001_R1_001.fastq.gz HolEw3-1a_S1_L001_R2_001.fastq.gz \
+reads_trim_R1.fastq reads_unpair_R1.fastq \
+reads_trim_R2.fastq reads_unpair_R2.fastq \
+LEADING:10 TRAILING:10
+
+# trimming long reads
+
+filtlong --min_length 1000 --keep_percent 90 all_pacbio_echium.fastq | gzip > all_pacbio_echium.trimmed.fastq &&
+flye \
+--pacbio-hifi all_pacbio_echium.trimmed.fastq \
+--out-dir assembly_hifi \
+--genome-size 509m \
+--threads 20 \
+--scaffold \
+
+
+
+
+
